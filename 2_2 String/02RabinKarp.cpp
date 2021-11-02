7b611fa3e5cb83152e1af0a20a19539d19e27900
Anmol Verma <anmolverma102002@gmail.com>
2021-11-02 11:43:48 +0530

    Started Rabin Karp Algo

---
+++ 2_2 String/02RabinKarp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Rabin Karp Algo use the Hash function also called Rolling hash function as the kmp algo use the prefix function or pi function 
+// Always use large prime number in the hash function to reduce collision or in this case know as Spurious Hits
+
+/*
+    pat -> pattern
+    txt -> text
+    q   -> prime number
+*/
+
+
+int main()
+{
+    
+    return 0;
+}
