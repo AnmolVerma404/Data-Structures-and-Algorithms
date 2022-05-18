a96010e79cfe792164b20839a2737464eb0bab25
Anmol Verma <anmolverma102002@gmail.com>
2022-05-18 19:33:20 +0530

    Introduction to Graphs

---
+++ 6 Graph ADT/Concept/00Introduction.cpp
@@ -0,0 +1,18 @@
+/*
+What is Graph Data Structure?
+It is a nonlinear data structure to store inter-related or connected data.
+It consists of edges and vertices.
+
+Vertices - A node where data and information about connected nodes are stored
+Edges - It defines the connection b/w two nodes
+
+Graphs are further characterized into two types - Directed and Undirected Graph
+Undirected Graph - This type of graph has two edges b/w every connected vertex, traversal from both vertex to the other vertex is possible.
+Directed Graph - In this type of Graph there are at most two edges b/w every connected vertex. Traversal from node a to node b may be possible but visa versa might not be possible.
+
+InDegree of a graph - The number of edges directed towards the node.
+OutDegree of a graph - The number of vertices the node is pointing towards
+
+Cycle in a Graph - In a traversal of a graph if starting node is once again visited then we can assume it's a cyclic graph Eg - a->b->c->a is a cyclic graph.
+Acyclic graph - A graph that does not contain a cycle. Most of the Directed graphs are Acyclic
+*/
