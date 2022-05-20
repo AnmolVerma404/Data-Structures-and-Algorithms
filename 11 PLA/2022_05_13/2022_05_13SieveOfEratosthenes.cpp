600da692a0be1bbaf5e09e2df28810938dc524d6
Anmol Verma <anmolverma102002@gmail.com>
2022-05-20 15:44:06 +0530

    Added PLA Code

---
+++ 11 PLA/2022_05_13/2022_05_13SieveOfEratosthenes.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void soe(int n)
+{
+    bool isPrime[n + 1];
+    memset(isPrime, true, sizeof(isPrime));
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (isPrime[i] == true)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (isPrime[i] == true)
+        {
+            cout << i << " ";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    soe(n);
+    return 0;
+}
---
+++ 11 PLA/2022_05_18/gcd.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    cout<<__gcd(24,27);
+    return 0;
+}
