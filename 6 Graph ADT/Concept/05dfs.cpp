79000f3e255178050c3f19418be9910a7fbb0ab0
Anmol Verma <anmolverma102002@gmail.com>
2022-05-22 16:24:43 +0530

    DFS Graph Code Added

--- 6 Graph ADT/Concept/05dfs.cpp
+++ 6 Graph ADT/Concept/05dfs.cpp
@@ -5,13 +5,35 @@
 
 using namespace std;
 
-void dfs(vector<vector<int>>&v,int n){
-    
+void search(vector<vector<int>> &v, bool visited[], int i, int n)
+{
+    visited[i] = true;
+    cout<<i<<" ";
+    for (auto j : v[i])
+    {
+        if (visited[j] != true)
+        {
+            search(v, visited, j, n);
+        }
+    }
+}
+
+void dfs(vector<vector<int>> &v, int n)
+{
+    bool visited[n + 1];
+    memset(visited, false, sizeof(visited));
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] != true)
+        {
+            search(v, visited, i, n);
+        }
+    }
 }
 
 int main()
 {
-    //Make sure to take a vaild graph
+    // Make sure to take a vaild graph
     vector<vector<int>> g = {
         {1, 5},
         {0, 2, 5, 3},
@@ -20,6 +42,6 @@ int main()
         {3, 5},
         {0, 1, 4}};
     int size = g.size();
-    dfs(g,size);
+    dfs(g, size);
     return 0;
 }
