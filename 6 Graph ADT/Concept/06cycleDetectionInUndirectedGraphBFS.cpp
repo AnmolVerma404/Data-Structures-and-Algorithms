d8359e934d02013196858a46b24ff137e4eece8c
Anmol Verma <anmolverma102002@gmail.com>
2022-05-23 18:56:26 +0530

    Cycle Detection In Undirected Graph BFS with explanation

--- 6 Graph ADT/Concept/06cycleDetectionInUndirectedGraphBFS.cpp
+++ 6 Graph ADT/Concept/06cycleDetectionInUndirectedGraphBFS.cpp
@@ -1,9 +1,15 @@
+/*
+To detect a cycle in a graph mean - If BFS/DFS traversal to a node which is already visited then it may be due to presence of a cycle in the graph.
+To know whether a cycle is present we use a parent pointer which shows parent connection.
+In Adjcancy list we can store the next connection and the parent connection in a pair
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
 bool bfs(vector<vector<int>> g)
 {
+    //Everything similar to BFS traversal
     int n = g.size();
     int visited[n];
     memset(visited, false, sizeof(visited));
@@ -11,8 +17,8 @@ bool bfs(vector<vector<int>> g)
     {
         if (!visited[i])
         {
-            queue<pair<int, int>> q;
-            q.push({i, -1});
+            queue<pair<int, int>> q;//Instead of int take pair<int,int> which will store the next connection and the parent connection for a node
+            q.push({i, -1});//For the initital node set the parent to -1
             while (!q.empty())
             {
                 int node = q.front().first;
@@ -26,7 +32,7 @@ bool bfs(vector<vector<int>> g)
                         q.push({j, node});
                         visited[j] = true;
                     }
-                    else if (par != j)
+                    else if (par != j)//If the curr node is visited and it's the visited node's parent is not same as the current node parent return true i.e. cycle is present
                     {
                         return true;
                     }
