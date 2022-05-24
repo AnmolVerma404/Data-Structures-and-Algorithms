e0ade905ccf06be39a43dde22baa2b907d52fe2c
Anmol Verma <anmolverma102002@gmail.com>
2022-05-24 10:27:41 +0530

    Move Hyphen To Beginning

--- 11 PLA/2022_05_24/moveHyphenToBeginning.java
+++ 11 PLA/2022_05_24/moveHyphenToBeginning.java
@@ -1,5 +1,20 @@
 public class moveHyphenToBeginning {
     public static void main(String[] args) {
-        
+        String s = "__Hello_World_";
+        char[] str = s.toCharArray();
+        moveHyphen(str);
+        System.out.println(String.valueOf(str));
+    }
+
+    public static void moveHyphen(char[] str) {
+        int i = str.length - 1;
+        for (int j = i; j >= 0; j--) {
+            if (str[j] != ' ') {
+                char ch = str[i];
+                str[i] = str[j];
+                str[j] = ch;
+                i--;
+            }
+        }
     }
 }
--- 11 PLA/2022_05_24/weightesSubstring.java
+++ 11 PLA/2022_05_24/weightesSubstring.java
@@ -2,13 +2,16 @@ public class weightesSubstring {
     public static void main(String[] args) {
         String s = "HelloWorld";
         String wt = "4987653215";
+        // String wt = "1111111111";
         int k = 20;
-        wtSubstring(s, wt, k);
+        // wtSubstring(s, wt, k);//
+        opWtSubstring(s, wt, k);//29
     }
 
     public static void wtSubstring(String s, String wt, int k) {
         int n = s.length();
         int m = wt.length();
+        int tsbs = 0;
         for (int i = 0; i < n; i++) {
             String ss = "";
             int sum = 0;
@@ -17,9 +20,23 @@ public class weightesSubstring {
                 int w = wt.charAt(j) - '0';
                 sum += w;
                 if (sum <= k) {
+                    tsbs++;
                     System.out.println(ss);
                 }
             }
         }
+        System.out.println(tsbs);
+    }
+    public static void opWtSubstring(String s, String wt, int k){
+        int n = s.length();
+        int m = wt.length();
+        int tw = 0;
+        for(int i = 0;i<n;i++){
+            tw+= (wt.charAt(i)-'0');
+        }
+        for(int i = m-1;i>=0;i--){
+            
+        }
+        // System.out.println(tw);
     }
 }
