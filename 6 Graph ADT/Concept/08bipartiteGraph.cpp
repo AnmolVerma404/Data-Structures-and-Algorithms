2fa6da55db02e8c6dea70a958db48431b24c468d
Anmol Verma <anmolverma102002@gmail.com>
2022-05-24 23:02:00 +0530

    Added bipartite Graph explanation

--- 6 Graph ADT/Concept/08bipartiteGraph.cpp
+++ 6 Graph ADT/Concept/08bipartiteGraph.cpp
@@ -1,3 +1,8 @@
+/*
+In simple words bipartite mean divided into two parts.
+This problem can be easily understand and solve by assuming that each node is a graph can be colored as blue(1) ,red(0), none(-1). If after traversal we found that a single node have different color then the graph is not a bipartite graph
+Or we can say that a graph whose vertices can be divided into two disjoint and independent set U and V .Equivalently, a bipartite graph is a graph that does not contain any odd-length cycles.
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,23 +13,23 @@ bool isBipartite(vector<vector<int>> g)
     vector<int> color(n, -1); // visited color:- 0->red, 1->blue
     for (int i = 0; i < n; i++)
     {
-        if (color[i] == -1)
+        if (color[i] == -1)//If the node is not visited then only it will have color none(-1)
         {
             queue<int> q;
             q.push(i);
-            color[i] = 1;
+            color[i] = 1;//visited node put 1
             while (!q.empty())
             {
                 int top = q.front();
                 q.pop();
                 for (auto j : g[top])
                 {
-                    if (color[j] == -1)
+                    if (color[j] == -1)//If the node is not visited then only it will have color none(-1)
                     {
-                        q.push(j);
-                        color[j] = 1 - color[top];
+                        q.push(j);//Add the connection to the queue
+                        color[j] = 1 - color[top];//if 1->0 || 0->1 this is how every next node will have different color
                     }
-                    else if (color[j] == color[top])
+                    else if (color[j] == color[top])//If the color of the next node and the current node are same i.e. two different color on the same node return false
                     {
                         return false;
                     }
@@ -32,7 +37,7 @@ bool isBipartite(vector<vector<int>> g)
             }
         }
     }
-    return true;
+    return true;//Finally after bfs traversal return true 
 }
 
 int main()
@@ -44,11 +49,6 @@ int main()
         {1, 2, 4},
         {3, 5},
         {0, 1, 4}};
-    // vector<vector<int>> g = {
-    //     {1},
-    //     {2, 3},
-    //     {1, 4},
-    //     {1, 4}};
     cout << isBipartite(g);
     return 0;
 }
