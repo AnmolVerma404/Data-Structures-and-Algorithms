6063ec1a2148ad292c5bfa426cfa5fe8c5c00b69
Anmol Verma <anmolverma102002@gmail.com>
2022-05-25 09:36:37 +0530

    Maneuvering A Cave and Sorted Unique Permutation

---
+++ 11 PLA/2022_05_25/maneuveringACave.java
@@ -0,0 +1,26 @@
+
+public class maneuveringACave {
+    public static void main(String[] args) {
+        int n = 5, m = 4;
+        int[][] dp = new int[n + 1][m + 1];
+        System.out.println(numberOfPath(n, m));
+        System.out.println(numberOfPathDP(dp, n, m));
+    }
+
+    public static int numberOfPathDP(int[][] dp, int n, int m) {
+        if (m == 1 || n == 1) {
+            return dp[n][m] = 1;
+        }
+        if (dp[n][m] == 0) {
+            dp[n][m] = numberOfPathDP(dp, n - 1, m) + numberOfPathDP(dp, n, m - 1);
+        }
+        return dp[n][m];
+    }
+
+    public static int numberOfPath(int n, int m) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        return numberOfPath(n - 1, m) + numberOfPath(n, m - 1);
+    }
+}
---
+++ 11 PLA/2022_05_25/sortedUniquePermutation.java
@@ -0,0 +1,41 @@
+public class sortedUniquePermutation {
+    public static void main(String[] args) {
+        String str = "";
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        printComb(arr, 4);
+    }
+
+    /*
+    public static void perm(int[] arr,String s){
+        if(s.length() >= 2){
+            System.out.println(s);
+            return;
+        }
+        for(var i : arr){
+            s+=i;
+            System.out.println(s);
+            perm(arr,s);
+            s = "";
+        }
+        return;
+    }
+    */
+    public static void printComb(int[] arr, int r) {
+        int[] data = new int[r];
+        combUtil(arr, data, 0, arr.length - 1, 0, r);
+    }
+
+    public static void combUtil(int[] arr, int[] data, int start, int end, int idx, int r) {
+        if (idx == r) {
+            for (int i = 0; i < r; i++) {
+                System.out.print(data[i]);
+            }
+            System.out.println("");
+            return;
+        }
+        for (int i = start; i <= end && end - i + 1 >= r - idx; i++) {
+            data[idx] = arr[i];
+            combUtil(arr, data, i + 1, end, idx + 1, r);
+        }
+    }
+}
