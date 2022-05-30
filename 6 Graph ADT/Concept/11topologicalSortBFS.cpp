dabce36f19c44eada9aa6360ec5bf7ff1067d0f9
Anmol Verma <anmolverma102002@gmail.com>
2022-05-30 22:21:47 +0530

    Topological Sort BFS explanation

--- 6 Graph ADT/Concept/11topologicalSortBFS.cpp
+++ 6 Graph ADT/Concept/11topologicalSortBFS.cpp
@@ -1,37 +1,46 @@
+/*
+Topological Sort using DFS traversal algo can be done using an extra array containing all the indegree's of a node.
+Why need of an extra array?
+As we know in BFS all the adjacent node are explored and in DFS we explore the first not visited node and continue to do it, by that in dfs we can keep track of one single path whereas in BFS we are exploring all the adjacent node so we need an extra infromation to get the information about how many node are connected to current node.
+As we know inDegree give information about how many node are direction towards the current node, and in topological sort in a graph of edge from u to v, u should always come before v.
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
 vector<int> topoBFS(vector<vector<int>> &g, int n)
 {
-    queue<int> q;
-    vector<int> indegree(n, 0);
+    queue<int> q;//Define a queue 
+    vector<int> indegree(n, 0);//Defina a Indegree list 
+    /*
+    Logic behind indegree is all the node directing towards the curr node therefor we will increment the jth index of indegree of size = number of vertex in the graph.
+    */
     for (int i = 0; i < n; i++)
     {
         for (auto j : g[i])
         {
-            indegree[j]++;
+            indegree[j]++;//Add all nodes indegree
         }
     }
     for (int i = 0; i < n; i++)
     {
-        if (indegree[i] == 0)
+        if (indegree[i] == 0)//If indegree of an vertex in an graph is 0 add it into the queue
             q.push(i);
     }
-    vector<int> topo;
-    while (!q.empty())
+    vector<int> topo;//This will store the result
+    while (!q.empty())//Run until the queue is not empty
     {
         int node = q.front();
         q.pop();
-        topo.push_back(node);
-        for (auto j : g[node])
+        topo.push_back(node);//Push the top of queue in the topo vector
+        for (auto j : g[node])//find all the connected node to current node and decrement the indegree as the current node is indegree is 0 and it's been added to topo list
         {
             indegree[j]--;
-            if (indegree[j] == 0)
+            if (indegree[j] == 0)//If the jth element indegree is 0 add it into the queue for used later
                 q.push(j);
         }
     }
-    return topo;
+    return topo;//Finally return the list
 }
 
 
@@ -47,7 +56,7 @@ int main()
         {0,2},
     };
     int n = g.size();
-    vector<int> topo = topoBFS(g, n);
+    vector<int> topo = topoBFS(g, n);//Call topo BFS and store the topological sort in a list
     for (auto i : topo)
         cout << i << " ";
     return 0;
