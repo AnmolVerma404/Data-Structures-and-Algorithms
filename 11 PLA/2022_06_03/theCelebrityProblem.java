9e693a1b53a71b4f78384160b06d5eae59b9643a
Anmol Verma <anmolverma102002@gmail.com>
2022-06-03 09:56:07 +0530

    Celebrity Prob

--- 11 PLA/2022_06_03/theCelebrityProblem.java
+++ 11 PLA/2022_06_03/theCelebrityProblem.java
@@ -1,5 +1,47 @@
+import java.util.Stack;
+
 public class theCelebrityProblem {
+    static boolean knows(int a, int b, int[][] m) {
+        return m[a][b] == 1 ? true : false;
+    }
+
+    static int findCeleb(int[][] m) {
+        int n = m.length;
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; ++i) {
+            st.push(i);
+        }
+        while (st.size() > 1) {
+            int a = st.pop();
+            int b = st.pop();
+            if (knows(a, b, m)) {
+                st.push(b);
+            } else {
+                st.push(a);
+            }
+        }
+        if (st.empty()) {
+            return -1;
+        }
+        int c = st.pop();
+        for (int i = 0; i < n; i++) {
+            if (i != c && (knows(c, i, m) || !knows(i, c, m))) {
+                return -1;
+            }
+        }
+        return c;
+    }
+
     public static void main(String[] args) {
-        
+        int m[][] = { { 0, 0, 1, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 1, 0 } };
+        int res = findCeleb(m);
+        if (res == -1) {
+            System.out.println("No Celebrity Found");
+        } else {
+            System.out.println("Celebrity found at " + res);
+        }
     }
 }
