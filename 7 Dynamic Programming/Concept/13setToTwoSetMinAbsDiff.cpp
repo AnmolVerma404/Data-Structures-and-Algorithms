ba2fc8a7d1c1938f8e1b4043a9ab4905c6d58a3d
Anmol Verma <anmolverma102002@gmail.com>
2022-07-14 16:54:26 +0530

    Partition A Set Into Two Subsets With Minimum Absolute Sum Difference Explanation Tabulation

--- 7 Dynamic Programming/Concept/13setToTwoSetMinAbsDiff.cpp
+++ 7 Dynamic Programming/Concept/13setToTwoSetMinAbsDiff.cpp
@@ -1,3 +1,8 @@
+/*
+Problem Link - https://leetcode.com/problems/partition-array-into-two-arrays-to-minimize-sum-difference/
+Same as previous, new approach in main function
+Only Tabulation Written
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -27,26 +32,34 @@ void targetSumTabulationDP(int n, int t, vector<int> &v, vector<vector<int>> &dp
 
 int main()
 {
+    //It's hard to observe that how this problem relate to last
+    //In the last problem we were saving boolean value that if at a row i,t can be successful
+    //We we know at a target if it's possible
+    //Therefor if we sum up this problem array, and set the target to it
+    //We will have n*(sum+1) size array, also at the (n-1)th index we will have if the sum is possible or not
+    //And we can take the difference of totalSum and sum of ith from i = 0 to i<totalSum
+    //And can get the minimum abs difference sum
     vector<int> v = {1, 2, 3, 4};
     int n = v.size();
-    int totalSun = 0;
+    int totalSum = 0;
     for (auto &it : v)
-        totalSun += it;
-    int t = totalSun;
+        totalSum += it;//Sum up 
+    int t = totalSum;//Set t = totalSum and the prev tabluation way is same
 
-    vector<vector<int>> dp(n, vector<int>(t + 1, 0));
+    vector<vector<int>> dp(n, vector<int>(t + 1, 0));//Set to t+1, where t = totalSum
+
     targetSumTabulationDP(n, t, v, dp);
 
     int mn = 1e9;
-    for (int s1 = 0; s1 < totalSun; ++s1)
+    for (int s1 = 0; s1 < totalSum; ++s1)
     {
         if (dp[n - 1][s1] == true)
         {
-            mn = min(mn, abs((totalSun - s1) - s1));
+            mn = min(mn, abs((totalSum - s1) - s1));//Find the minimum
         }
     }
 
-    cout << mn << "\n";
+    cout << mn << "\n";//Print the minimum abs difference sum
 
     return 0;
 }
