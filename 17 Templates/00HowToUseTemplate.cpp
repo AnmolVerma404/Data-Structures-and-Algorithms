af4a25ef5b53d6d3d7634bebd10d3af1a53f13fc
Anmol Verma <anmolverma102002@gmail.com>
2023-04-29 12:34:19 +0530

    feat: :plus: added DisjointSet Template

---
+++ 17 Templates/00HowToUseTemplate.cpp
@@ -0,0 +1,49 @@
+/*
+ * You can either copy the class code(header file aka .h) inside the template file.
+ * Or paste the file in your folder and #include "fileName.h" and use the class
+ * Here is the example how to use DisjointSet.h class
+ * If the .h file is not in the same directory, you can use #include "../fileName.h" for upper directory.
+ * Or use #include "./folderName/fileName.h" for lower directory
+ */
+
+#include "DisjointSet.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printArray(vector<int> &v)
+{
+    for (auto &it : v)
+        cout << it << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    DisjointSet ds = DisjointSet(7);
+    ds.unionByRank(1, 2);
+    ds.unionByRank(2, 3);
+    ds.unionByRank(4, 5);
+    ds.unionByRank(6, 7);
+    ds.unionByRank(5, 6);
+    if (ds.findParent(3) == ds.findParent(7))
+    {
+        cout << "Same\n";
+    }
+    else
+    {
+        cout << "Not Same\n";
+    }
+    ds.unionByRank(3, 7);
+    if (ds.findParent(3) == ds.findParent(7))
+    {
+        cout << "Same\n";
+    }
+    else
+    {
+        cout << "Not Same\n";
+    }
+
+    ds.unionByRank(1, 2);
+    return 0;
+}
---
+++ 17 Templates/DisjointSet.h
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+    vector<int> rank, parent;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i <= n; ++i)
+        {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ultimateParentU = findParent(u);
+        int ultimateParentV = findParent(v);
+        if (ultimateParentU == ultimateParentV)
+            return;
+
+        if (rank[ultimateParentU] < rank[ultimateParentV])
+        {
+            parent[ultimateParentU] = ultimateParentV;
+        }
+        else if (rank[ultimateParentV] < rank[ultimateParentU])
+        {
+            parent[ultimateParentV] = ultimateParentU;
+        }
+        else
+        {
+            parent[ultimateParentV] = ultimateParentU;
+            rank[ultimateParentU]++;
+        }
+    }
+};
